156ff7dcb369077e61cd16f0d797e9f6fafb56f2
Gabriel Taillon <ga.taillon@gmail.com>
2021-08-04 16:54:17 +0900

    Adds import script.

--- Import Tileset.lua
+++ Import Tileset.lua
@@ -4,13 +4,12 @@ if TilesetMode == nil then return app.alert "Use Aseprite v1.3"  end
 -- Layer name should be same name as in the Tileset_*layername*.png 
 -- For Every Tilemap Layer:
 -- 1. Create empty Tilemap *layername*_2
--- 2. Import .png pixels. (create many new tiles)
--- 3. Delete all tile indices on the canvas
--- 4. Copy pixels from *layername*
--- 5. Delete *layername*
--- 6. Rename *layername*_2 to *layername* 
-
-local row_len = 8
+-- 2. Import .png tileset. 
+-- 3. Copy tileset pixels pixels.
+-- 4. Delete all tile indices/pixels on the canvas
+-- 5. Copy pixels from *layername*
+-- 6. Delete *layername*
+-- 7. Rename *layername*_2 to *layername* 
 
 local spr = app.activeSprite
 local lay = app.activeLayer
@@ -22,8 +21,6 @@ if string.find(output_folder, "Maps")  then
 end
 -- print(tileset_folder)
 if not lay.isTilemap then return app.alert "No active tilemap layer" end
-
-
 
 local function getLayer(spr, layername)
   local outlayer
@@ -38,9 +35,10 @@ end
 
 local function layer_tilemap_create(layername)
   app.command.NewLayer()
+  app.command.ConvertLayer{["to"]="tilemap"}
+  -- app.command.NewLayer{["tilemap"]=true} -- Borks placement offset Aseprite Issue #2743
   layer = app.activeLayer
   layer.name = layername .. "_2"
-  app.command.ConvertLayer{["to"]="tilemap"}
   return(layer)
 end
 layernames = {}
@@ -52,24 +50,42 @@ for i = 1, (#layernames) do
   -- print(layernames[i])
   local tileset_filename = app.fs.joinPath(tileset_folder, "Tileset_"..layernames[i]..".png")
   layer_tilemap_create(layernames[i])
-  local oldlayer = getLayer(spr, layernames[i])
   local newlayer = getLayer(spr, layernames[i].."_2")
-  if newlayer == nil then
-    print("newlayer is nil")
-  end
-  if oldlayer == nil then
-    print("oldlayer is nil")
-  end
+  local oldlayer = getLayer(spr, layernames[i])
+  assert(newlayer ~= nil)
+  assert(oldlayer ~= nil)
+  oldlayer.isVisible = true
+  newlayer.isVisible = true
   sprite_tileset = app.open(tileset_filename)
-  -- local selection_temp = Selection()
+
+  -- Copy all pixels from tileset_filename to newlayer (creates new tiles)
   sprite_tileset.selection:selectAll()
   app.command.Copy()
   app.activeSprite = spr
   app.activeLayer = newlayer
-  spr.selection:selectAll()
+  app.command.ToggleTilesMode()
+  app.command.TilesetMode{["mode"]="auto"}
   app.command.Paste()
+  
+  -- Delete all pixels in newlayer
+  app.command.ToggleTilesMode()
+  spr.selection:selectAll()
+  app.command.TilesetMode{["mode"]="stack"}
+  app.command.Clear()
+  sprite_tileset.selection:deselect()
   sprite_tileset:close()
+
+  -- Copy pixels from oldlayer
+  spr = app.activeSprite
+  -- app.activeSprite = spr
+  app.activeLayer = oldlayer
+  spr.selection:selectAll()
+  app.command.Copy()
+  app.activeLayer = newlayer
+  -- spr.selection:selectAll()
+  spr.selection:deselect()
+  app.command.Paste()
+  spr.selection:deselect()
 end
 
-  -- local newlayer = layer_tilemap_create(layer.name)
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scripts for Aseprite v1.3-beta (tilemap editor)
 
-These Lua scripts automate Tileset and Tilemap export.
+These Lua scripts automate Tileset and Tilemap import/export.
 
 Both ```Export Tilemap.lua``` and ```Export Tileset.lua``` are modified versions of scripts by [David Capello](https://github.com/dacap).
 Credit to [rxi](https://github.com/rxi/json.lua) for ```json.lua```.
@@ -9,3 +9,5 @@ Everything including my modifications are under the MIT licence, feel free to us
 - ```Export Tilemap.lua``` uses json.lua to export aseprite tile indices for each tilemap layer. The exported active area is visible in aseprite by holding the ctrl key. Let me repeat: Exported tilemaps are *NOT* for the whole canvas. I don't know how to change this behavior. 
 
 - ```Export Tileset.lua``` exports a tileset into a rectangular ```png``` with a maximal ```row_len```, by copying and flattening all layers. I use it to create tiles with different backgrounds.
+
+- ```Import Tileset.lua``` uses layer names of all tilemap layers to import tilesets. For each tilemap layer, a new tilemap layer is created, the tileset is imported from the ```Tileset_*layer_name*.png``` file, then the pixels from the old tilemap layers are copied over. Finally, the old tilemap layers are discarded and the new ones renamed to their respective names.
